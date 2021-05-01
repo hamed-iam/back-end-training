c934ae4dc89ea9005bb935e2947cd8227de4f11b
Hamed Ghazali <hamedg20@yahoo.com>
2021-05-01 12:21:21 +0430

    model done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
 const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const { notFound, errorHandler } = require('./middleware');
+require('dotenv').config();
 
 const app = express();
-
-app.use(morgan('common'));
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
 app.use(helmet());
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-  })
-);
+app.use(morgan('common'));
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'HELLLOOO THEREE FROM HAMED !',
+  });
+});
+
+app.use(notFound);
+
+app.use(errorHandler);
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
+});
